2732ff25e2ba980653454dc9b8f41b999a1101db
Maria-PC\School <maria.haddad@mail.mcgill.ca>
2026-05-27 11:18:17 -0400

    v1 - Changed the folder structure to data -> participant ID -> session date -> trial number

--- data_logging.py
+++ data_logging.py
@@ -4,10 +4,16 @@ import csv
 import glob
 
 
-def make_folder(data_folder: str, participant: str) -> str:
+def make_folder(
+    data_folder: str,
+    participant: str,
+    session: str | None = "",
+    trial: str | None = "",
+) -> str:
     """
     Within the folder where all data is saved, creates a sub-folder for current
-    participant if it does not exist already.
+    participant (and session and trial, if specified) if it does not exist
+    already.
 
     Parameters
     ----------
@@ -15,13 +21,18 @@ def make_folder(data_folder: str, participant: str) -> str:
         Base folder containing data for all participants.
     participant : str
         Participant identifier number.
+    session : str
+        Current session number, if specified
+    trial: str
+        Current trial number, if specified
 
     Returns
     -------
     folder : str
-        Sub-folder specific to this participant.
+        Sub-folder specific to this participant (and session and trial, if
+                                                 specified).
     """
-    folder = os.path.join(data_folder, participant)
+    folder = os.path.join(data_folder, participant, session, trial)
     if not os.path.exists(folder):
         os.makedirs(folder)
     print("Preparing to save onto folder ", folder)
@@ -30,7 +41,7 @@ def make_folder(data_folder: str, participant: str) -> str:
 
 def get_session(folder: str) -> str:
     """
-    Parses files already in current participant's sub-folder to identify the
+    Parses folders already in current participant's sub-folder to identify the
     current session number. If no sessions have been recorded for this
     participant, the number is set to 0.
 
@@ -45,16 +56,38 @@ def get_session(folder: str) -> str:
         Current session number.
     """
     # determining which session number to write to (to not over-write data)
-    files = glob.glob(os.path.join(folder, "*.csv"))
+    files = glob.glob(os.path.join(folder, "*"))
     if (len(files)) > 0:
-        sessions = [
-            int(file.split("\\")[-1].split("_")[0].split("S")[1])
-            for file in files
-        ]
-        session = str(max(sessions) + 1)
+        session = str(len(files) - 1)
     else:
         session = "0"
     return session
+
+
+def get_trial(folder: str) -> str:
+    """
+    Parses folders already in current participant's session sub-folder to
+    identify the current trial number. If no trial have been recorded for this
+    participant, the number is set to 0.
+
+    Parameters
+    ----------
+    folder : str
+        Sub-folder corresponding to current participant.
+
+    Returns
+    -------
+    trial : str
+        Current trial number.
+    """
+    # determining which session number to write to (to not over-write data)
+    files = glob.glob(os.path.join(folder, "*"))
+    if (len(files)) > 0:
+        trials = [int(file.split("\\T")[-1]) for file in files]
+        trial = str(max(trials) + 1)
+    else:
+        trial = "0"
+    return trial
 
 
 def make_headers(
@@ -105,6 +138,7 @@ def make_headers(
 
 def make_filenames(
     session: str,
+    trial: str,
     scene: str,
     data_to_save: list[bool],
     data_types: list[str] | None = [
@@ -122,6 +156,8 @@ def make_filenames(
     ----------
     session : str
         Current session number.
+    trial : str
+        Current trial number.
     scene : str
         Current playable scene selected (6 options).
     data_to_save : list of bool
@@ -145,6 +181,9 @@ def make_filenames(
                 + session
                 + "_"
                 + str(date.today())
+                + "_"
+                + "T"
+                + trial
                 + "_"
                 + scene
                 + "_"
@@ -210,22 +249,36 @@ def create_files(
     None
     """
     folder = make_folder(arg["folder"], arg["participant"])
+
+    session_folder = make_folder(
+        arg["folder"], arg["participant"], session=str(date.today())
+    )
     session = get_session(folder)
+    trial = get_trial(session_folder)
+
+    trial_folder = make_folder(
+        arg["folder"],
+        arg["participant"],
+        session=str(date.today()),
+        trial="T" + trial,
+    )
 
     data_to_save = [arg[data_types[i]] for i in range(len(data_types))]
-    filenames = make_filenames(session, arg["scene"], data_to_save)
+
+    filenames = make_filenames(session, trial, arg["scene"], data_to_save)
     headers = make_headers(data_to_save)
 
     for i in range(len(filenames)):
-        make_csv(folder, filenames[i], headers[i])
+        make_csv(trial_folder, filenames[i], headers[i])
         print("Created the file " + filenames[i])
 
 
 def find_files(
-    folder: str, scene: str, participant: str
+    folder: str, scene: str, participant: str, trial: str
 ) -> tuple[list[str], str]:
     """
-    SUMMARY.
+    Find files pertaining to the current trial in-progress within the sub-folder
+    containing data for this participant's current recording session.
 
     Parameters
     ----------
@@ -235,40 +288,46 @@ def find_files(
         Selected playable scene for current session.
     participant : str
         Current participant identifier.
+    trial : str
+        Current trial number.
 
     Returns
     -------
-    session_files : list of str
-        Files pertaining to the current session in-progress.
-    session_basename : str
-        Common base for all kept files.
+    trial_files : list of str
+        Files pertaining to the current trial in-progress.
+    trial_basename : str
+        Common base for all files pertaining to this trial.
     """
     # get latest session only
     if os.path.exists(folder):
         # setting up the data logging folder and participant name
-        data_folder = os.path.join(folder, participant)
+        data_folder = os.path.join(
+            folder, participant, str(date.today()), trial
+        )
         files = glob.glob(os.path.join(data_folder, "*.csv"))
-        sessions = [
-            int(file.split("\\")[-1].split("_")[0].split("S")[1])
+        trials = [
+            int(file.split("\\")[-1].split("_T")[1].split("_")[0])
             for file in files
         ]
-        if len(sessions) > 0:
-            session = str(max(sessions))
+        if len(trials) > 0:
+            trial = str(max(trials))
         else:
-            session = "0"
-        session_files = glob.glob(
+            trial = "0"
+        trial_files = glob.glob(
             os.path.join(
-                data_folder, "*S" + session + "_" + str(date.today()) + "*.csv"
+                data_folder, "*" + str(date.today()) + "_T" + trial + "*.csv"
             )
         )
 
-        session_basename = session_files[0].rsplit("_", 1)[0] + "_"
+        trial_basename = trial_files[0].rsplit("_", 1)[0] + "_"
     else:
         print("The data-saving folder selected does not exist.")
-    return session_files, session_basename
+    return trial_files, trial_basename
 
 
-def save_file(base: str, timestamp: str, data_type: str, data_values: str) -> None:
+def save_file(
+    base: str, timestamp: str, data_type: str, data_values: str
+) -> None:
     """
     Opens already-created file pertaining to this data type and saves values.
 
@@ -315,7 +374,8 @@ def save_data(
     ],
 ) -> None:
     """
-    SUMMARY.
+    One by one, opens files pertaining to current participant, session, and
+    trial, and save a new line of data.
 
     Parameters
     ----------
@@ -338,8 +398,18 @@ def save_data(
     -------
     None
     """
-    session_files, session_basename = find_files(
-        arg["folder"], arg["scene"], arg["participant"]
+    trial = str(
+        int(
+            get_trial(
+                os.path.join(
+                    arg["folder"], arg["participant"], str(date.today())
+                )
+            )
+        )
+        - 1
+    )
+    trial_files, trial_basename = find_files(
+        arg["folder"], arg["scene"], arg["participant"], "T" + trial
     )
     data_to_save = {key: arg[key] for key in data_types if key in arg}
 
@@ -347,7 +417,7 @@ def save_data(
         # only save if data was received
         if (list(data_to_save.values())[i + 1]) is not None:
             save_file(
-                session_basename,
+                trial_basename,
                 data_to_save["time"],
                 list(data_to_save.keys())[i + 1],
                 list(data_to_save.values())[i + 1],

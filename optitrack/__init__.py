cf6929c044b984af191b1033de00e0287580b7b8
Félix Chénier <chenier.felix@uqam.ca>
2026-06-11 21:35:01 -0400

    [main] Added a unit test template, and solved a few problems in optitrack module following code checks.

--- optitrack/__init__.py
+++ optitrack/__init__.py
@@ -30,15 +30,15 @@ __license__ = "Apache 2.0"
 
 import sys
 import time
-from .NatNetClient import NatNetClient
 import kineticstoolkit.lab as ktk
 import numpy as np
+from .NatNetClient import NatNetClient
 
 # Maximal number of frames to keep in memory
-frame_limit = 10000
+FRAME_LIMIT = 10000
 
 # Initialize empty lists to store positions, orientations and timestamps
-_data = {}
+_data: dict[int, dict[str, list]] = {}
 ts = {}
 
 # Global variable for the NatNet client
@@ -47,7 +47,7 @@ _streaming_client = NatNetClient()
 
 def receive_rigid_body_frame(
     new_id: int, position: np.ndarray, orientation: np.ndarray
-) -> None:  # ignore: D401
+) -> None:
     """
     Add a new received rigid body data (used as callback).
 
@@ -59,10 +59,6 @@ def receive_rigid_body_frame(
         The position of the rigid body as a NumPy array.
     orientation
         The orientation of the rigid body as a NumPy array.
-
-    Returns
-    -------
-    None
 
     """
     # Get UNIX TimeStamps for the absolute_time
@@ -82,19 +78,19 @@ def receive_rigid_body_frame(
     _data[new_id]["_times"].append(absolute_time)
 
     # If frame count exceeds limit, remove oldest frames
-    if len(_data[new_id]["_positions"]) > frame_limit:
+    if len(_data[new_id]["_positions"]) > FRAME_LIMIT:
         _data[new_id]["_positions"].pop(0)
         _data[new_id]["_orientations"].pop(0)
         _data[new_id]["_times"].pop(0)
 
 
-def fetch() -> ktk.TimeSeries:
+def fetch() -> dict[str, ktk.TimeSeries]:
     """
     Get the trajectory of all received rigid bodies as TimeSeries of transforms.
 
     Returns a dictionary of TimeSeries where each key is the ID of the rigid
     body in Motive, and the TimeSeries contains one transform series. The max
-    length of these TimeSeries can be changed by changing the frame_limit
+    length of these TimeSeries can be changed by changing the FRAME_LIMIT
     property of this module.
 
     A dictionary of TimeSeries is returned instead of one TimeSeries because
@@ -107,20 +103,18 @@ def fetch() -> ktk.TimeSeries:
 
     """
 
-    for key in _data.keys():
+    for key, value in _data.items():
 
         n_samples = min(
-            len(_data[key]["_times"]),
-            len(_data[key]["_positions"]),
-            len(_data[key]["_orientations"]),
+            len(value["_times"]),
+            len(value["_positions"]),
+            len(value["_orientations"]),
         )
 
         # Convert lists of times, positions and orientations to numpy arrays
-        times_np = np.array(_data[key]["_times"][:n_samples])
-        positions_np = np.array(_data[key]["_positions"][:n_samples])[:, 0:3]
-        orientations_np = np.array(_data[key]["_orientations"][:n_samples])[
-            :, 0:4
-        ]
+        times_np = np.array(value["_times"][:n_samples])
+        positions_np = np.array(value["_positions"][:n_samples])[:, 0:3]
+        orientations_np = np.array(value["_orientations"][:n_samples])[:, 0:4]
 
         # Create the homogeneous transformation matrix
         transforms = ktk.geometry.create_transform_series(
@@ -136,14 +130,7 @@ def fetch() -> ktk.TimeSeries:
 
 
 def start() -> None:
-    """
-    Start receiving data from NatNet.
-
-    Returns
-    -------
-    None
-
-    """
+    """Start receiving data from NatNet."""
     # Configure client to receive rigid body data
     _streaming_client.rigid_body_listener = receive_rigid_body_frame
 
@@ -165,25 +152,11 @@ def start() -> None:
 
 
 def stop() -> None:
-    """
-    Stop receiving data from NatNet.
-
-    Returns
-    -------
-    None
-
-    """
+    """Stop receiving data from NatNet."""
     # Stop NatNet client
     _streaming_client.shutdown()
 
 
 def clear() -> None:
-    """
-    Clear buffer.
-
-    Returns
-    -------
-    None
-
-    """
+    """Clear buffer."""
     _data.clear()
---
+++ tests/test_modulename.py
@@ -0,0 +1,24 @@
+"""
+Template for pytest.
+
+Copy this file and change modulename for the module to be tested, then modify
+test_functionname and maybe add other test functions.
+
+"""
+
+
+def test_functionname():
+    """
+    Test functionname.
+
+    Change this for your test and add as required functions that start with "test_",
+    they will all be tested using pytest.
+    """
+    return
+    # Make the function crash in case of error, for example by using assert x == y
+
+
+if __name__ == "__main__":  # pragma: no cover
+    import pytest
+
+    pytest.main([__file__])

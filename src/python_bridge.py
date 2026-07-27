47faa548c43d84de95d2db580dc7c6244c2d14d7
Maria-PC\School <maria.haddad@mail.mcgill.ca>
2026-07-27 06:47:11 -0400

    Merging main into #9

--- src/python_bridge.py
+++ src/python_bridge.py
@@ -1,26 +1,11 @@
 """
-Execute python commands with arguments, repeatedly or once.
+Main entry point for Godot to launch the Python bridge.
 
-This script opens a port (PYTHON_PORT) and listens for JSON strings of this
-form:
-    {
-        "command": str,
-        "args": any
-        "run_mode": "once", "start" or "stop"
-    }
-
-For run_mode == "once", the function listed in COMMAND_MAPPING[command] is
-executed once.
-
-For run_mode == "start", the function listed in COMMAND_MAPPING[command] starts
-being executed continuously. Many functions can be started at the same time;
-in this case they are executed one after the other, continuously.
-
-For run_mode == "stop", the function listed in COMMAND_MAPPING[command] stops
-being executed consinuously.
-
+This script acts as a lightweight wrapper that initializes and starts
+the UDP receiver loop to handle communication with Godot.
 """
 
+<<<<<<< HEAD
 import json
 import os
 import socket
@@ -82,58 +67,9 @@ def send_data(command, data):
     json_message = json.dumps(message).encode("utf-8")
     _private_vars["sock"].sendto(json_message, (UDP_IP, GODOT_PORT))
 
+=======
+from bridge_package import receiver
+>>>>>>> main
 
 if __name__ == "__main__":
-    _init_udp_socket()
-
-    # Sending ping request, availables functions to Godot for debug scene
-    print("Python connected to Godot...\n")
-    time.sleep(1)
-    send_data("ping", list(COMMAND_MAPPING.keys()))
-
-    time.sleep(1)
-
-    # Listening Godot requests
-    while _private_vars["is_running"]:
-        # Execute every command in the UDP buffer
-        while True:  # until there's nothing available anymore
-            try:
-                message, address = _private_vars["sock"].recvfrom(1024)
-
-                command_dict = json.loads(message.decode("utf-8"))
-                command = command_dict["command"]
-                run_mode = command_dict["run_mode"]
-                args = command_dict["args"]
-
-                if run_mode == "start":
-                    if command not in _running_commands:
-                        _running_commands[command] = {"args": args}
-
-                elif run_mode == "stop":
-                    if command in _running_commands:
-                        _running_commands.pop(command)
-
-                elif run_mode == "once":
-                    COMMAND_MAPPING[command](command_dict["args"])
-
-                else:
-                    raise ValueError(
-                        "frequency must be 'start', 'stop' or 'once'"
-                    )
-
-            except BlockingIOError:
-                break
-            except ConnectionResetError:
-                break
-
-        # Do not execute repeating commands after shutdown request
-        if not _private_vars["is_running"]:
-            break
-
-        # Execute every repeating command
-        for command in _running_commands:
-            COMMAND_MAPPING[command](_running_commands[command]["args"])
-
-    # Quit
-    _private_vars["sock"].close()
-    os._exit(0)
+    receiver.start()

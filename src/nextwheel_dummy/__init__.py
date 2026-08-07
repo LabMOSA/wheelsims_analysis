ea5e3aa1f2565a4ed3d66f8716972d85f47b7f79
Félix Chénier <chenier.felix@uqam.ca>
2026-08-07 15:00:08 -0400

    [30-dummy-nextwheel] #30 Implemented nextwheel_dummy, and added a constructor unit test.

---
+++ src/nextwheel_dummy/__init__.py
@@ -0,0 +1,65 @@
+"""
+Provide NextWheel dummy data, like if we had a real NextWheel.
+
+This is a package mainly for testing. It mimicks a very small subset of the
+real NextWheel class so that we can instantiate a wheel, start streaming (in
+reality it's always "streaming"), and fetch data.
+
+"""
+
+import nextwheel
+import time
+import kineticstoolkit as ktk
+import os
+import numpy as np
+
+
+this_dir = os.path.dirname(__file__)
+
+
+class NextWheel:
+    def __init__(self):
+
+        # Read dummy data
+        self._data = nextwheel.read_dat(
+            this_dir + "/sample.dat", this_dir + "/sample_calibration.json"
+        )
+
+        # Set the time zero around the middle of this series
+        shift = self._data["Analog"].time.mean() + 20
+        self._data["Analog"].shift(-shift, in_place=True)
+        self._data["IMU"].shift(-shift, in_place=True)
+        self._data["Encoder"].shift(-shift, in_place=True)
+        self._data["Power"].shift(-shift, in_place=True)
+
+        self._time_start = time.time()
+
+    def start_streaming(self):
+        """Do nothing."""
+        pass
+
+    def stop_streaming(self):
+        """Do nothing."""
+        pass
+
+    def fetch(self):
+        """Fetch data as if we were connected to a real wheel."""
+        time_now = time.time()
+        if time_now - self._time_start > self._data["Analog"].time[-1]:
+            # We reached the end of the data and should restart at zero.
+            self._time_start = time_now
+
+        return {
+            "Analog": self._data["Analog"].get_ts_between_times(
+                time_now - self._time_start - 60.0, time_now - self._time_start
+            ),
+            "IMU": self._data["IMU"].get_ts_between_times(
+                time_now - self._time_start - 60.0, time_now - self._time_start
+            ),
+            "Encoder": self._data["Encoder"].get_ts_between_times(
+                time_now - self._time_start - 60.0, time_now - self._time_start
+            ),
+            "Power": self._data["Power"].get_ts_between_times(
+                time_now - self._time_start - 60.0, time_now - self._time_start
+            ),
+        }
